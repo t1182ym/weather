276b51931637eb17f114da72f45d1bb4896820df
t1182ym <t11828ym@gmail.com>
2015-06-17 13:36:08 +0900

    画面表示

---
+++ weather/WXClient.h
@@ -0,0 +1,13 @@
+//
+//  WXClient.h
+//  weather
+//
+//  Created by Yuta Makino on 2015/06/16.
+//  Copyright (c) 2015年 Yuta Makino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WXClient : NSObject
+
+@end
---
+++ weather/WXDailyForecast.m
@@ -0,0 +1,23 @@
+//
+//  WXDailyForecast.m
+//  weather
+//
+//  Created by Yuta Makino on 2015/06/16.
+//  Copyright (c) 2015年 Yuta Makino. All rights reserved.
+//
+
+#import "WXDailyForecast.h"
+
+@implementation WXDailyForecast
+
++ (NSDictionary *)JSONKeyPathsByPropertyKey {
+    // 1
+    NSMutableDictionary *paths = [[super JSONKeyPathsByPropertyKey] mutableCopy];
+    // 2
+    paths[@"tempHigh"] = @"temp.max";
+    paths[@"tempLow"] = @"temp.min";
+    // 3
+    return paths;
+}
+
+@end
